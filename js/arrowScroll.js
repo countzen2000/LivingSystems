8d9957550c89e751969ca735339212617ed72685
Billy Shin <countzen@gmail.com>
2015-04-03 17:36:52 -0700

    Lots-o-Changes

--- js/arrowScroll.js
+++ js/arrowScroll.js
@@ -2,8 +2,8 @@ arrowScroll = (function(){
 	var currentPosition = 0;
 	var positionArray = [
 		'0',
-		'2100',
-		'3500',
+		'7000',
+		'13000',
 		'#section4',
 		'#section5',
 		'#section6',
@@ -11,44 +11,103 @@ arrowScroll = (function(){
 		'#section8'
 		]
 
-		var gotoNextMenu = function () {
-			currentPosition++;
-			if (currentPosition > positionArray.length-1)
-			{
-				currentPosition = positionArray.length-1;
-			}
-			livingSys.controller.scrollTo(positionArray[currentPosition]);
-			console.log(currentPosition);
+	var gotoNextMenu = function () {
+		console.log(currentPosition);
+		currentPosition++;
+		if (currentPosition > positionArray.length-1)
+		{
+			currentPosition = positionArray.length-1;
+		}
+		//livingSys.controller.scrollTo(positionArray[currentPosition]);
+		//console.log(currentPosition);
+		var targetPos = positionArray[currentPosition];
+		if (isNaN(targetPos))
+		{
+			targetPos = $(targetPos).offset().top;
+		}
+		if (currentPosition < 2) {
+			TweenMax.to(window, 2, {scrollTo:{y:targetPos}, ease:Power2.easeOut});
+		} else {
+			TweenMax.to(window, 1, {scrollTo:{y:targetPos}, ease:Power2.easeOut});
+		}
+	}
+
+	var gotoPreviousMenu = function() {
+		console.log(currentPosition);
+		currentPosition--;
+		if (currentPosition < 0)
+		{
+			currentPosition = 0;
+		}
+		//livingSys.controller.scrollTo(positionArray[currentPosition]);
+		//console.log(currentPosition);
+		var targetPos = positionArray[currentPosition];
+		if (isNaN(targetPos))
+		{
+			targetPos = $(targetPos).offset().top;
 		}
 
-		var gotoPreviousMenu = function() {
-			currentPosition--;
-			if (currentPosition < 0)
-			{
-				currentPosition = 0;
-			}
-			livingSys.controller.scrollTo(positionArray[currentPosition]);
-			console.log(currentPosition);
+		if (currentPosition < 2) {
+			TweenMax.to(window, 2, {scrollTo:{y:targetPos}, ease:Power2.easeOut});
+		} else {
+			TweenMax.to(window, 1, {scrollTo:{y:targetPos}, ease:Power2.easeOut});
 		}
+	}
 
-		var init = function() {
-			$(document).keydown(function(e) {
-			    switch(e.which) {
-			        case 38: // up
-			        	gotoPreviousMenu();
-			        	break;
+	var init = function() {
+		$(document).keydown(function(e) {
+			currentPosition = whereAreYou();
+		    switch(e.which) {
+		        case 38: // up
+		        	gotoPreviousMenu();
+		        	break;
 
-			        case 40: // down
-			        	gotoNextMenu();
-	        			break;
+		        case 40: // down
+		        	gotoNextMenu();
+        			break;
 
-			        default: return; // exit this handler for other keys
-			    }
-			    e.preventDefault(); // prevent the default action (scroll / move caret)
-			});
+		        default: return; // exit this handler for other keys
+		    }
+		    e.preventDefault(); // prevent the default action (scroll / move caret)
+		});
+	}
+
+	var whereAreYou = function() {
+		var location = $(window).scrollTop();
+		console.log(location)
+		var returnValue;
+		switch (true) {
+			case (location == 0):
+				returnValue = 0;
+				break;
+			case (location > 0 && location <= positionArray[1]):
+				returnValue =  1;
+				break;
+			case (location >= positionArray[1] && location <= positionArray[2]):
+				returnValue =  2;
+				break;
+			case (location >= positionArray[2] && location <= $(positionArray[3]).offset().top):
+				returnValue = 3;
+				break;
+			case (location >= $(positionArray[3]).offset().top && location <= $(positionArray[4]).offset().top):
+				returnValue = 4;
+				break;
+			case (location >= $(positionArray[4]).offset().top && location <= $(positionArray[5]).offset().top):
+				returnValue = 5;
+				break;
+			case (location >= $(positionArray[5]).offset().top && location <= $(positionArray[6]).offset().top):
+				returnValue = 6;
+				break;
+			case (location >= $(positionArray[6]).offset().top):
+				returnValue = 7;
+				break;
 		}
+		return returnValue;
+	}
 
 		return {
-			init:init
+			init:init,
+			positionArray:positionArray,
+			whereAreYou:whereAreYou
 		}
 }())
--- js/livingSys.js
+++ js/livingSys.js
@@ -1,25 +1,7 @@
 livingSys = (function() {
 	
 	var controller = new ScrollMagic.Controller();
-
-	/******public functioncs *****/
-	var init = function() {
-		setupAnimation();
-		realign();
-
-		/* Scrolling stuff ======================================================== */
-		controller.scrollTo(function (newpos) {
-			console.log("what in the world?!"+newpos);
-			TweenMax.to( window, 1,
-					{
-						scrollTo: { y: newpos },
-						ease: Cubic.easeInOut,
-						autoKill: false
-					}
-				);
-		});
-		/* Scrolling stuff ======================================================== */
-	}
+	var scrollLocation = [];
 
 	var setupAnimation = function() {
 
@@ -34,12 +16,12 @@ livingSys = (function() {
 				TweenMax.to("#pin1", 1, {"alpha":0, top: "-=25"})
 			)
 			.add(
-				TweenMax.to("#section1", 1, {transformOrigin: "50% 50% 0", scale: .75, alpha:0})
+				TweenMax.to("#section1", 1.5, {transformOrigin: "50% 50% 0", scale: .75, alpha:0})
 			).add(
-				TweenMax.from("#section2", .5, {transformOrigin: "50% 50% 0", scale: 1.5, alpha:0})
+				TweenMax.from("#section2", .75, {transformOrigin: "50% 50% 0", scale: 1.5, alpha:0}, '-1')
 			)
 
-		var fadeOutText1 = new ScrollMagic.Scene({offset: 200, duration: 1500})
+		var fadeOutText1 = new ScrollMagic.Scene({offset: 200, duration: 6000})
 						.setTween(tween1)
 						.addTo(controller);
 		fadeOutText1.addIndicators();
@@ -60,12 +42,12 @@ livingSys = (function() {
 				TweenMax.to("#pin2", 1, {"alpha":0, top: "-=25"})
 			)
 			.add(
-				TweenMax.to("#section2", 1, {transformOrigin: "50% 50% 0", scale: .75, "alpha":0})
+				TweenMax.to("#section2", 1.5, {transformOrigin: "50% 50% 0", scale: .75, "alpha":0})
 			).add(
-				TweenMax.from("#section3", .5, {transformOrigin: "50% 50% 0", scale: 1.5, alpha:0})
+				TweenMax.from("#section3", .75, {transformOrigin: "50% 50% 0", scale: 1.5, alpha:0})
 			)
 
-		var fadeOutText2 = new ScrollMagic.Scene({offset: 1700, duration: 1500})
+		var fadeOutText2 = new ScrollMagic.Scene({offset: 6200, duration: 6000})
 						.setTween(tween2)
 						.addTo(controller);
 		fadeOutText2.addIndicators();
@@ -83,31 +65,67 @@ livingSys = (function() {
 				TweenMax.to("#pin3", 2, {})
 			)
 			.add(
-				TweenMax.to("#pin3", 1, {"alpha":0, top: "-=25"})
+				TweenMax.to("#pin3", 1.5, {"alpha":0, top: "-=25"})
 			).add(
-				TweenMax.to("#section3", 1, {"alpha":0})
+				TweenMax.to("#section3", .75, {"alpha":0})
 			)
 
-		var fadeOutText3 = new ScrollMagic.Scene({offset: 3200, duration: 1500})
+		var fadeOutText3 = new ScrollMagic.Scene({offset: 12200, duration: 6000})
 						.setTween(tween3)
 						.addTo(controller);
 		fadeOutText3.addIndicators();
+
 	}
 
-	var resize = function() {
-		
+	var onResize = function() {
+
+		//Fix for section 5
+		var container = $("#section5_top");
+		var work_container = $("#work-list-container");
+		var parent = container.parent();
+
+		container.offset({top:parent.offset().top + parent.height()/2 - container.height()/2-50});
+		work_container.offset({top:parent.offset().top +parent.height() - 470});
+
 	}
 
 	var realign = function() {
-		$('#section4').offset({top: 4700});
+		//$('#section6').height($('#section6').height() + 50);
+
+		$('#section4').offset({top: 18200});
 		$('#section5').offset({top: $('#section4').offset().top + $('#section5').height()});
-		$('#section6').offset({top: $('#section5').offset().top + $('#section6').height() + 200});
-		$('#section7').offset({top: $('#section6').offset().top + $('#section7').height() + 100});
-		$('#section8').offset({top: $('#section7').offset().top + $('#section8').height() + 100});
+		$('#section6').offset({top: $('#section5').offset().top + $('#section6').height()+50});
+		$('#section7').offset({top: $('#section6').offset().top + $('#section7').height()});
+		$('#section8').offset({top: $('#section7').offset().top + $('#section8').height()});
 		
 		var section4 = $('#section4').offset().top;
 		$('#page-header').offset({top: section4});
 	}
+
+/******public functioncs *****/
+	var init = function() {
+		setupAnimation();
+		realign();
+
+		/* Scrolling stuff ======================================================== */
+		controller.scrollTo(function (newpos) {
+			console.log("what in the world?!"+newpos);
+			TweenMax.to( window, 2,
+					{
+						scrollTo: { y: newpos },
+						ease: Cubic.easeInOut,
+						autoKill: false
+					}
+				);
+		});
+		/* Scrolling stuff ======================================================== */
+
+		/*** resizing stuff **/
+
+		$(window).resize(onResize);
+		onResize();
+	}
+
 	return {
 		init: init,
 		controller: controller
